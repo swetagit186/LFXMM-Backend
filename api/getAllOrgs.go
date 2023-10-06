b02226764388476a61a5fb29ba0ca6eb6baa403c
swetagit186 <sweta.kumari.eee22@itbhu.ac.in>
2023-10-06 09:40:25 +0530

    changes in getAllOrgs

--- api/getAllOrgs.go
+++ api/getAllOrgs.go
@@ -11,5 +11,32 @@ func getAllOrgs(c *gin.Context) {
 	client := handlers.New()
 	defer client.Close()
 
-	c.IndentedJSON(http.StatusOK, client.GetAllParentOrgs())
+	year := c.Param("year")
+	term := c.Param("term")
+
+	// if year== nil && term==nil {
+	// 	c.IndentedJSON(http.StatusOK, client.GetAllParentOrgs())
+	// }
+	// else if year==nil {
+	// 	c.IndentedJSON(http.StatusOK, client.GetAllParentOrgsByTerm())
+	// }
+	// else if(term==nil){
+	// 	c.IndentedJSON(http.StatusOK, client.GetAllParentOrgsByYear())
+	// }
+	// else{
+	// 	c.IndentedJSON(http.StatusOK, client.GetAllParentOrgs())
+	// }
+
+	if year=="" && term=="" {
+        c.IndentedJSON(http.StatusOK, client.GetAllParentOrgs())
+    } else if year=="" {
+        c.IndentedJSON(http.StatusOK, client.GetAllParentOrgsByTerm())
+    } else if term==""{
+    	c.IndentedJSON(http.StatusOK, client.GetAllParentOrgsByYear())
+    }else{
+		c.IndentedJSON(http.StatusOK, client.GetAllParentOrgsByTermYear())
+	}
+
+
+	
 }
